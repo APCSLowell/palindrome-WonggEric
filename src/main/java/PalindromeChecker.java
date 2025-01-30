ebb363d0996161045199afbab4aac357857171b5
WonggEric <88013940+WonggEric@users.noreply.github.com>
2025-01-29 23:19:31 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -48,7 +48,7 @@ public class PalindromeChecker {
   public String reverse(String str) {
     String sNew = "";
     for (int i = str.length() - 1; i >= 0; i--) {
-      result += str.charAt(i);
+      sNew += str.charAt(i);
     }
     return sNew;
   }
